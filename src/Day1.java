e5e449f3a2804bacdc80ba33fff6e0519d408d37
JoshBarrette <joshpbarrette@gmail.com>
2024-12-02 09:08:16 -0600

    Day 1 part 2

--- src/Day1.java
+++ src/Day1.java
@@ -4,26 +4,37 @@ public class Day1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        ArrayList<Integer> left = new ArrayList<>();
-        ArrayList<Integer> right = new ArrayList<>();
-        int total = 0;
+        HashMap<Integer, int[]> map = new HashMap<>();
 
         while (true) {
-            String temp = scan.nextLine();
-            if (temp.isEmpty()) {
+            String tempLine = scan.nextLine();
+            if (tempLine.isEmpty()) {
                 break;
             }
 
-            String[] split = temp.split(" {3}");
-            left.add(Integer.parseInt(split[0]));
-            right.add(Integer.parseInt(split[1]));
+            String[] split = tempLine.split(" {3}");
+            int left = Integer.parseInt(split[0]);
+            int right = Integer.parseInt(split[1]);
+
+            int[] tempLeft = map.get(left);
+            if (tempLeft == null) {
+                map.put(left, new int[] {1, 0});
+            } else {
+                map.replace(left, new int[] { tempLeft[0] + 1, tempLeft[1] });
+            }
+
+            int[] tempRight = map.get(right);
+            if (tempRight == null) {
+                map.put(right, new int[] {0, 1});
+            } else {
+                map.replace(right, new int[] { tempRight[0], tempRight[1] + 1 });
+            }
         }
 
-        Collections.sort(left);
-        Collections.sort(right);
-
-        for (int i = 0; i < left.size(); i++) {
-            total += Math.abs(left.get(i) - right.get(i));
+        int total = 0;
+        for (int key : map.keySet()) {
+            int[] thing = map.get(key);
+            total += key * thing[0] * thing[1];
         }
 
         System.out.println("total: " + total);
