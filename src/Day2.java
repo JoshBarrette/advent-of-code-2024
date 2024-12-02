0931040926db254f181237571d7584b41ac911df
JoshBarrette <joshpbarrette@gmail.com>
2024-12-02 13:17:46 -0600

    Day 2 part 2

--- src/Day2.java
+++ src/Day2.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day2 {
@@ -13,14 +12,13 @@ public class Day2 {
             if (tempLine.isEmpty()) {
                 break;
             }
-
             String[] split = tempLine.split(" ");
-            int[] temp = new int[split.length];
-            for (int i =0; i < split.length; i++) {
-                temp[i] = Integer.parseInt(split[i]);
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            for (String s : split) {
+                temp.add(Integer.parseInt(s));
             }
 
-            if (reportIsSafe(temp)) {
+            if (reportIsSafe(temp, false)) {
                 total++;
             }
         }
@@ -28,18 +26,29 @@ public class Day2 {
         System.out.println("total: " + total);
     }
 
-    public static boolean reportIsSafe(int[] report) {
-        if (report[0] == report[1]) return false;
-        boolean isPos = report[0] - report[1] > 0;
+    public static boolean reportIsSafe(ArrayList<Integer> report, boolean levelRemoved) {
+        boolean isPos = report.get(0) - report.get(1) > 0;
 
-        for (int i = 0; i < report.length - 1; i++) {
-            int diff = report[i] - report[i + 1];
-            int absDiff = Math.abs(report[i] - report[i + 1]);
-            if (diff > 0 != isPos) {
-                return false;
-            } else if (absDiff < 1 || absDiff > 3) {
-                return false;
+        for (int i = 0; i < report.size() - 1; i++) {
+            int diff = report.get(i) - report.get(i + 1);
+            int absDiff = Math.abs(report.get(i) - report.get(i + 1));
+            
+            if (diff > 0 == isPos && absDiff >= 1 && absDiff <= 3) {
+                continue;
             }
+            else if (levelRemoved) return false;
+            else if (i == report.size() - 2) return true;
+
+            ArrayList<Integer> temp;
+            for (int j = 0; j < report.size(); j++) {
+                temp = (ArrayList<Integer>) report.clone();
+                temp.remove(j);
+                if (reportIsSafe(temp, true)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         return true;
