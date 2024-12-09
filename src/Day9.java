e4210aaf57e9a993b5fd13e953aa6042e7f6cb16
JoshBarrette <joshpbarrette@gmail.com>
2024-12-09 10:06:51 -0600

    Day 9 part 2

--- src/Day9.java
+++ src/Day9.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day9 {
-    private static ArrayList<int[]> arr = new ArrayList<>(); // [isData, fileID]
+    private static ArrayList<int[]> arr = new ArrayList<>(); // [isData, fileID, size]
     private static boolean isData = true;
     private static int fileID = 0;
 
@@ -20,19 +20,17 @@ public class Day9 {
                     .forEach((s) -> parseCell(Integer.parseInt(s)));
         }
 
-        printData();
-        reOrderData();
-        printData();
+//        printData();
+        reOrderDataPart2();
+//        printData();
         System.out.println("total: " + countData());
     }
 
     public static long countData() {
         long total = 0;
         for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i)[0] == 0) {
-                return total;
-            } else {
-                total += (long) i * arr.get(i)[1];
+            if (arr.get(i)[0] == 1) {
+                total += (long) i * (long) arr.get(i)[1];
             }
         }
 
@@ -60,12 +58,46 @@ public class Day9 {
         }
     }
 
+    public static void reOrderDataPart2() {
+        for (int right = arr.size() - 1; right >= 0; right--) {
+            int[] rightItem = arr.get(right);
+            int size = rightItem[2];
+
+            if (rightItem[0] == 0) continue;
+
+            for (int left = 0; left < right - size; left++) {
+                int[] leftItem = arr.get(left);
+
+                if (leftItem[0] == 1 || leftItem[2] < size) {
+                    continue;
+                }
+
+                int emptySpace = leftItem[2];
+
+                for (int idx = 0; idx < size; idx++) {
+                    int[] temp = arr.get(left + idx);
+                    arr.set(left + idx, arr.get(right - idx));
+                    arr.set(right - idx, temp);
+                }
+
+                for (int i = left + size; i < left + emptySpace; i++) {
+                    if (arr.get(i)[0] == 1) break;
+                    arr.get(i)[2] -= size;
+                }
+
+                right -= size - 1;
+
+                break;
+            }
+        }
+    }
+
     public static void parseCell(int cell) {
         for (int i = 0; i < cell; i++) {
             if (isData) {
-                arr.add(new int[]{1, fileID});
+                arr.add(new int[]{1, fileID, cell});
             } else {
-                arr.add(new int[]{0, 0});
+                arr.add(new int[]{0, 0, cell});
             }
         }
 
